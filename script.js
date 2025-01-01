a864ff8eb5be938d70c1bcfc87fd9aa7c97f856f
Albert Humphreys <rashanon.humphreys@icloud.com>
2025-01-01 15:59:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ MenuBtn.addEventListener("click", function (e) {
   document.querySelector("body").classList.toggle("mobile-nav-active");
   this.classList.toggle("fa-xmark");
 });
+
+// Typing effect
+
+let typed = new Typed(".auto-input", {
+  strings: ["", "Developer.", "Designer."],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
